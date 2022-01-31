0d5340d7fde00be1c49b59786d661c222994fb09
Mia Moretti <mimickal.dev@gmail.com>
2022-01-30 19:27:58 -0500

    Better asserts and catches in basic KSY test

--- test/static_ksy_test.py
+++ test/static_ksy_test.py
@@ -6,7 +6,10 @@ import yaml
 ksy_files = dict()
 for path in Path('../h1').rglob('*.ksy'):
 	with open(path) as yaml_file:
-		ksy_files[path.stem] = yaml.safe_load(yaml_file)
+		ksy_files[path.stem] = {
+			'path': path,
+			'ksy': yaml.safe_load(yaml_file),
+		}
 
 # Passing in just the filename improves test failure reporting.
 # If we pass the data in directly, pytest will print the whole data dict on test
@@ -15,22 +18,38 @@ for path in Path('../h1').rglob('*.ksy'):
 def test_static_ksy(filename):
 	"""Ensures static data is present in all ksy files"""
 
-	ksy = ksy_files[filename]
+	fileinfo = ksy_files[filename]
+	ksy = fileinfo['ksy']
+
 	meta = ksy.get('meta')
+	assert meta
+
 	assert meta.get('application') == 'Halo Custom Edition'
 	assert meta.get('license')     == 'GPL-3.0-only'
 	assert 'Gearbox Halo CE' in ksy.get('doc')
 
-	if filename != 'common':
-		xref = meta.get('xref')
-		assert xref.get('tag_id') or xref.get('filename')
+	if is_top_level(filename):
+		assert meta.get('id') == filename
 		assert 'c20.reclaimers.net' in ksy.get('doc-ref')
 
-	if filename not in ('blam', 'common', 'savegame'):
-		assert meta.get('id')             == filename
-		assert meta.get('file-extension') == filename
-
 		xref = meta.get('xref')
-		assert xref.get('filename') is None
-		assert xref.get('tag_id') and len(xref.get('tag_id')) == 4
+		assert xref
+
+		if is_file(filename):
+			assert xref.get('filename')
+			assert xref.get('tag_id') is None
+
+		if is_tag(filename):
+			assert meta.get('file-extension') == filename
+			assert xref.get('filename') is None
+			assert xref.get('tag_id') and len(xref.get('tag_id')) == 4
+
+def is_top_level(filename):
+	return 'common' not in str(ksy_files[filename]['path'])
+
+def is_file(filename):
+	return filename in ('blam', 'savegame')
+
+def is_tag(filename):
+	return is_top_level(filename) and not is_file(filename)
 
